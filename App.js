81bd86bc28c549b3bc7f2fa0c076e2709ba950c7
elukuro <heruhartantot@gmail.com>
2022-01-24 21:10:00 +0700

    navigation setup

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
+/**
+ * stack navigator : classic back and forth between different nested screens  ex : moving from one screen to other
+ * bottom tab navigator : show a tab bar at the bottom with buttons to navigate between screen  ex: menu tab at bottom nav
+ * switch navigator : abrupt 100% cut between different screen  ex : login to homepage
+ * drawer navigator : shows fly-out menu from one side of the device ex : open menu
+ */
 export default function App() {
   return (
     <View style={styles.container}>
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ readme.md
@@ -0,0 +1,11 @@
+how to install navigation
+
+npm install react-navigation --legacy-peer-deps
+
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+npm install react-navigation-stack @react-native-community/masked-view --legacy-peer-deps
+
+npm install react-navigation-tabs --legacy-peer-deps
+
+expo r -c
