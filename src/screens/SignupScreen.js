45d605a54c31dc12ae654e3fa64500d5839b96ce
elukuro <heruhartantot@gmail.com>
2022-01-25 08:08:47 +0700

    singup form input props

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,24 +1,53 @@
-import react from "react";
+import react, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../Components/Spacer";
+import SigninScreen from "./SigninScreen";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-      <Spacer>
-        <Text h3>Sign up for Tracker</Text>
-      </Spacer>
-      <Input label="Email" />
-      <Spacer />
-      <Input label="Password" />
-      <Spacer>
-        <Button title="Sign Up" />
-      </Spacer>
+      <View style={styles.container}>
+        <Spacer>
+          <Text h3>Sign up for Tracker</Text>
+        </Spacer>
+        <Input
+          autoCompleteType={false}
+          autoCapitalize="none"
+          label="Email"
+          value={email}
+          onChangeText={(newEmail) => setEmail(newEmail)}
+        />
+        <Spacer />
+        <Input
+          autoCompleteType={false}
+          autoCapitalize="none"
+          label="Password"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={(newPassword) => setPassword(newPassword)}
+        />
+        <Spacer>
+          <Button title="Sign Up" />
+        </Spacer>
+      </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = {
+  headerShown: false,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 160,
+  },
+});
 
 export default SignupScreen;
