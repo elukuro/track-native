38e4729641e5cf2b5001d2eb90ab9ed3f1304624
elukuro <heruhartantot@gmail.com>
2022-02-05 16:54:53 +0700

    add request watching location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, SafeAreaView } from "react-native";
 import { Text } from "react-native-elements";
+import { requestForegroundPermissionsAsync } from "expo-location";
 
 import Map from "./../components/Map";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text h3>Create a Track</Text>
       <Map />
+      {err ? <Text>{err.message} please enable location service</Text> : null}
     </SafeAreaView>
   );
 };
@@ -21,3 +39,17 @@ const styles = StyleSheet.create({
 });
 
 export default TrackCreateScreen;
+
+// update funciton start watching
+// also need to import
+// import { requestForegroundPermissionsAsync } from 'expo-location';
+// const startWatching = async () => {
+//   try {
+//     const { granted } = await requestPermissionsAsync();
+//     if (!granted) {
+//       throw new Error('Location permission not granted');
+//     }
+//   } catch (e) {
+//     setErr(e);
+//   }
+// };
