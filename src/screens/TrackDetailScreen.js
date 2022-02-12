9bd736c8cc5a7b4f4227bb8885f4b23911ae97f7
elukuro <heruhartantot@gmail.com>
2022-02-12 17:38:59 +0700

    add map at detail

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Context as TrackContext } from "./../context/TrackContext";
+import MapView, { Polyline } from "react-native-maps";
 
 const TrackDetailScreen = ({ navigation }) => {
   const id = navigation.getParam("id");
@@ -8,11 +9,25 @@ const TrackDetailScreen = ({ navigation }) => {
   const track = state.find((track) => track._id === id);
   return (
     <View>
-      <Text>TrackDetailScreen {track}</Text>
+      <Text>TrackDetailScreen {track.name}</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...track.locations[0].coords,
+        }}
+      >
+        <Polyline coordinates={track.locations.map((loc) => loc.coords)} />
+      </MapView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
 
 export default TrackDetailScreen;
